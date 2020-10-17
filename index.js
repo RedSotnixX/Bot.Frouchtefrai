17fd5e0236c70d0b9607e6fc496129154602d1a9
RedSotnixX <72993807+RedSotnixX@users.noreply.github.com>
2020-10-17 11:35:11 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+  console.log("Le bot est connecté !")
+})
+
+bot.on('ready', function () {
+    bot.user.setActivity('rien').catch(console.error)
+})
+
+//bot.on('message', (message) => {
+  //if (message.content == '/muteAll') {
+      //let channel = message.member.voiceChannel;
+      //for (let member of channel) {
+        //  member[1].setMute(true)
+     // }
+  // }
+//});
+
+//bot.on('message', (message) => {
+  //if (message.content == '/muteAll') {
+    //  let channel = message.member.voiceChannel;
+    //  if(!channel){
+     //   return;
+     // }
+     // for (let member of channel.members) {
+     //     member.setMute(true);
+    //  }
+ //  }
+//});
+
+
+// KICK //
+
+//bot.on('message', message => {
+  //if (!message.guild) return;
+ // if (message.content.startsWith('fkick')) {
+   // const user = message.mentions.users.first();
+  //  if (user) {
+ //     const member = message.guild.member(user);
+ //     if (member) {
+     //   member
+ //         .voice.kick('Optional reason that will display in the audit logs')
+  //        .then(() => {
+  //          message.reply(`Successfully kicked ${user.tag}`);
+   //       })
+  //        .catch(err => {
+  //          message.reply('I was unable to kick the member');
+  //          console.error(err);
+  //        });
+  //    } else {
+   //     message.reply("That user isn't in this guild!");
+  //    }
+  //  } else {
+  //    message.reply("You didn't mention the user to kick!");
+ //   }
+ // }
+//});
+
+
+
+// Message de réponse //
+
+ bot.on('message', message => {
+     if (message.content === '/1') {
+       message.channel.send('et 2')
+     }
+   })
+
+
+
+  
+bot.login(process.env.BOT_TOKEN)
