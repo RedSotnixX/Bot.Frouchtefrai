af6211039b6be92c1062ed491b9aa801fa99d6d6
RedSotnixX <72993807+RedSotnixX@users.noreply.github.com>
2020-10-17 11:58:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+const CLEAR_MESSAGES = 'f/clear';
+
 bot.on('ready', function () {
   console.log("Le bot est connecté !")
 })
@@ -8,6 +10,47 @@ bot.on('ready', function () {
 bot.on('ready', function () {
     bot.user.setActivity('rien').catch(console.error)
 })
+
+bot.on('ready', () => {
+  console.log('ClearMessagesBot is Ready!');
+  bot.on('message', message => {
+    if (message.content == CLEAR_MESSAGES) {
+
+      // Check the following permissions before deleting messages:
+      //    1. Check if the user has enough permissions
+      //    2. Check if I have the permission to execute the command
+
+      if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
+        message.channel.sendMessage("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
+        console.log("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
+        return;
+      } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
+        message.channel.sendMessage("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
+        console.log("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
+        return;
+      }
+
+      // Only delete messages if the channel type is TextChannel
+      // DO NOT delete messages in DM Channel or Group DM Channel
+      if (message.channel.type == 'text') {
+        message.channel.fetchMessages()
+          .then(messages => {
+            message.channel.bulkDelete(messages);
+            messagesDeleted = messages.array().length; // number of messages deleted
+
+            // Logging the number of messages deleted on both the channel and console.
+            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
+            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+          })
+          .catch(err => {
+            console.log('Error while doing Bulk Delete');
+            console.log(err);
+          });
+      }
+    }
+  });
+});
+
 
 //bot.on('message', (message) => {
   //if (message.content == '/muteAll') {
