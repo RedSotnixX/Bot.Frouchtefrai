58643f5f3bd2d7a30e5cad3667c406978bee51c9
Red_StonixX <72993807+RedSotnixX@users.noreply.github.com>
2020-12-11 17:31:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ bot.on('ready', function () {
      }
    })
 
+bot.on('message', message => {
+     if (message.content === '1-G2') {
+       message.channel.send("Les 1-G1 c'est mieux")
+     }
+   })
+
  bot.on('message', message => {
      if (message.content === 'f/help') {
        message.channel.send('Les commandes disponibles sont ```f/1``` et ```f/help``` Pour Amoug us la commande help est ```f/au help```')
