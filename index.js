cd272b967aa06e089cf3044898b5eb918648f220
Red_StonixX <72993807+RedStonixX@users.noreply.github.com>
2020-12-14 17:16:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,22 +13,32 @@ bot.on("ready", async () => {
 
 // Message de réponse //
 
- bot.on('message', message => {
-     if (message.content === 'f/1') {
-       message.channel.send('et 2 et 3')
-     }
-   })
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
 
-bot.on('message', message => {
-     if (message.content === '1-G2') {
-       message.channel.send("Les 1-G1 c'est mieux")
-     }
-   })
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
 
- bot.on('message', message => {
-     if (message.content === 'f/help') {
-       message.channel.send('Les commandes disponibles sont ```f/1``` et ```f/help``` Pour Amoug us la commande help est ```f/au help```')
-     }
-   })
+    if(cmd === `${prefix}1`){
+        return message.channel.send("et 2 et 3")
+    }
+
+    if(cmd === `${prefix}help`){
+        return message.reply("Les commandes disponibles sont ```f/1``` et ```f/help``` Pour Amoug us la commande help est ```f/au help```")
+    }
+    
+// bot.on('message', message => {
+//     if (message.content === 'f/1') {
+//       message.channel.send('et 2 et 3')
+//     }
+//   })
+
+// bot.on('message', message => {
+//     if (message.content === 'f/help') {
+//       message.channel.send('Les commandes disponibles sont ```f/1``` et ```f/help``` Pour Amoug us la commande help est ```f/au help```')
+//     }
+//   })
   
 bot.login(process.env.BOT_TOKEN)
