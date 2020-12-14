531dbab40588b20d943a0c84bb67fb6992a8194d
Red_StonixX <72993807+RedStonixX@users.noreply.github.com>
2020-12-14 18:06:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,4 +40,31 @@ bot.on("ready", async () => {
      }
    })
   
+ bot.on('message', message => {
+     if(message.content === `${prefix}clear`){
+        if (message.deletable) {
+            message.delete();
+        }
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("Missing Permissions!").then(m => m.delete(5000));
+        }
+
+        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+            return message.reply("This is not a number").then(m => m.delete(5000));
+        }
+
+        let deleteAmount;
+        if (parseInt(args[0]) > 100) {
+            deleteAmount = 100;
+        } else {
+            deleteAmount = parseInt(args[0]);
+        }
+
+        message.channel.bulkDelete(deleteAmount, true)
+        .catch(err => message.reply(`Something went wrong... ${err}`));
+    
+    }
+ })
+
 bot.login(process.env.BOT_TOKEN)
