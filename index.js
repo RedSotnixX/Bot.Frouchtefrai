0d7a06e937d78ce7d498e49c9da58939244d7bdb
Red_StonixX <72993807+RedStonixX@users.noreply.github.com>
2020-12-14 16:59:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,10 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+const botsettings = require('./botsettings.json')
 
 bot.on('ready', function () {
   console.log("Le bot est connecté !")
 })
-
-
-//bot.on('message', (message) => {
-  //if (message.content == '/muteAll') {
-      //let channel = message.member.voiceChannel;
-      //for (let member of channel) {
-        //  member[1].setMute(true)
-     // }
-  // }
-//});
-
-//bot.on('message', (message) => {
-  //if (message.content == '/muteAll') {
-    //  let channel = message.member.voiceChannel;
-    //  if(!channel){
-     //   return;
-     // }
-     // for (let member of channel.members) {
-     //     member.setMute(true);
-    //  }
- //  }
-//});
-
-
-// KICK //
-
-//bot.on('message', message => {
-  //if (!message.guild) return;
- // if (message.content.startsWith('fkick')) {
-   // const user = message.mentions.users.first();
-  //  if (user) {
- //     const member = message.guild.member(user);
- //     if (member) {
-     //   member
- //         .voice.kick('Optional reason that will display in the audit logs')
-  //        .then(() => {
-  //          message.reply(`Successfully kicked ${user.tag}`);
-   //       })
-  //        .catch(err => {
-  //          message.reply('I was unable to kick the member');
-  //          console.error(err);
-  //        });
-  //    } else {
-   //     message.reply("That user isn't in this guild!");
-  //    }
-  //  } else {
-  //    message.reply("You didn't mention the user to kick!");
- //   }
- // }
-//});
-
 
 
 // Message de réponse //
@@ -76,18 +26,5 @@ bot.on('message', message => {
        message.channel.send('Les commandes disponibles sont ```f/1``` et ```f/help``` Pour Amoug us la commande help est ```f/au help```')
      }
    })
-
-//bot.on('message', message => {
-//     if (message.content === 'f/help') {
-//       message.channel.send('
-//Toute les commandes disponible son :
-//
-//***f/1*** dit et 2 et 3
-//***f/help*** Liste les commandes disponibles
-//
-// ')
-//     }
-//   })
-
   
 bot.login(process.env.BOT_TOKEN)
