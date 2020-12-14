dab17d635dbf419ea5821625258ae5885728ec9a
Red_StonixX <72993807+RedStonixX@users.noreply.github.com>
2020-12-14 17:05:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const bot = new Discord.Client()
+const bot = new Discord.Client({disableEveryone: true})
 const botsettings = require('./botsettings.json')
 
 bot.on('ready', function () {
