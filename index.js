b0ce2ebbfd986c9ea15979c060f4b7db5a56661c
Red_StonixX <72993807+RedStonixX@users.noreply.github.com>
2020-12-14 18:09:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,30 +41,9 @@ bot.on("ready", async () => {
    })
   
  bot.on('message', message => {
-     if(message.content === `${prefix}clear`){
-        if (message.deletable) {
-            message.delete();
-        }
-
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("Missing Permissions!").then(m => m.delete(5000));
-        }
-
-        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-            return message.reply("This is not a number").then(m => m.delete(5000));
-        }
-
-        let deleteAmount;
-        if (parseInt(args[0]) > 100) {
-            deleteAmount = 100;
-        } else {
-            deleteAmount = parseInt(args[0]);
-        }
-
-        message.channel.bulkDelete(deleteAmount, true)
-        .catch(err => message.reply(`Something went wrong... ${err}`));
-    
-    }
- })
+     if (message.content === `${prefix}serveur`) {
+       message.channel.send(`Le nom du serveur est : ${message.guild.name}`);
+     }
+   })
 
 bot.login(process.env.BOT_TOKEN)
