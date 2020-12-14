3c8ced01a1dc9712b18b72ed445bc4f1b776e017
Red_StonixX <72993807+RedStonixX@users.noreply.github.com>
2020-12-14 21:39:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,35 +2,56 @@ const Discord = require('discord.js')
 const config = require('./config.json');
 const { prefix } = require('./config.json');
 const bot = new Discord.Client({disableEveryone: true})
-const args = message.content.slice(prefix.length).trim().split(/ +/);
+const clear = require('./clear.js');
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online`)
     bot.user.setActivity("Space Engineers", {type: ""});
 })
 
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+bot.commands= new Discord.Collection();
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    bot.commands.set(command.name, command);
+}
+
+bot.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!bot.commands.has(command)) return;
+
+
+        try {
+            bot.commands.get(command).run(bot, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+
+// Message de réponse //
+
  bot.on('message', message => {
      if (message.content === `${prefix}1`) {
        message.channel.send('et 2 et 3')
      }
    })
-
-// Message de réponse //
-
- bot.on('message', message => {
-     if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-	 
-	 else if (command === 'kick') {
-	const taggedUser = message.mentions.users.first();
-
-	message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-	}
-    }
-})
-
 
  bot.on('message', message => {
      if (message.content === `${prefix}bot`) {
@@ -64,4 +85,4 @@ bot.on("ready", async () => {
      }
    })
 
-bot.login(process.env.BOT_TOKEN)
+bot.login('NzQzMTgyNTM2MTUwMDg5ODYw.XzQ8rA.HovXGm-I03yJMbQAgz_Kf7g6DYA')
