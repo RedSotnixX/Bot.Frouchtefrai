4682ef779c910910f3ec30f263fffbc0a74ecf9f
Red_StonixX <72993807+RedStonixX@users.noreply.github.com>
2020-12-15 09:14:07 +0100

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -5,16 +5,16 @@ module.exports = {
     description: "Converts text to ascii",
 
     async run (bot, message, args){
-        if(!args[0]) return message.channel.send('Please provide some text');
+        if(!args[0]) return message.channel.send('Merci de mettre un text');
 
         msg = args.join(" ");
 
         figlet.text(msg, function (err, data){
             if(err){
-                console.log('Something went wrong');
+                console.log('Il y a une erreur');
                 console.dir(err);
             }
-            if(data.length > 2000) return message.channel.send('Please provide text shorter than 2000 characters')
+            if(data.length > 2000) return message.channel.send('Merci de mettre un texte plus petit que 2000 characters')
 
             message.channel.send('```' + data + '```')
         })
