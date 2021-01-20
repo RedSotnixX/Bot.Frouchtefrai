17f63664a8ca48f47c292205a2cca1ba044dd41b
Red_StonixX <72993807+RedSotnixX@users.noreply.github.com>
2021-01-20 20:59:58 +0100

    Music avec lien

--- index.js
+++ index.js
@@ -93,5 +93,136 @@ bot.on('message', message => {
      }
    })
 
+
+
+const ytdl = require("ytdl-core");
+
+const queue = new Map();
+   
+bot.once("ready", () => {
+  console.log("Ready!");
+});
+   
+bot.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+   
+bot.once("disconnect", () => {
+  console.log("Disconnect!");
+});
+   
+bot.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+   
+  const serverQueue = queue.get(message.guild.id);
+   
+  if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}skip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+    return;
+  } else {
+    message.channel.send("La commande est invalide !");
+  }
+});
+   
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+   
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "Tu n'est pas dans un salon vocal !"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "Je n'est pas la permission de rejoindre et de jouer la musique :("
+    );
+  }
+   
+  const songInfo = await ytdl.getInfo(args[1]);
+  const song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url,
+  };
+   
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+   
+    queue.set(message.guild.id, queueContruct);
+   
+    queueContruct.songs.push(song);
+   
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} ajouté à la file d'attente !`);
+  }
+}
+   
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "Tu doit être dans un salon vocal pour passer la musique !"
+    );
+  if (!serverQueue)
+    return message.channel.send("Il n'y a pas de musique que je peux skip !");
+  serverQueue.connection.dispatcher.end();
+}
+   
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+   return message.channel.send(
+      "Tu doit être dans un salon vocal pour arreter la musique !"
+    );
+       
+  if (!serverQueue)
+    return message.channel.send("Il n'y a pas de musique que je peux arreter !");
+       
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+   
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+   
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+     serverQueue.songs.shift();
+     play(guild, serverQueue.songs[0]);
+    })
+   .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Musique en cours : **${song.title}**`);
+}
+
+
 bot.login(process.env.BOT_TOKEN)
 //bot.login('TOKEN')
