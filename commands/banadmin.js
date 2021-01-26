5440d59c030d5d13f75d29ae54ff3a7119ab3602
Red_StonixX <72993807+RedStonixX@users.noreply.github.com>
2021-01-26 19:02:08 +0100

    Update banadmin.js

--- commands/banadmin.js
+++ commands/banadmin.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: "ban",
+    name: "banadmin",
     description: "Banned a member from the server",
 
     async run (bot, message, args) {
