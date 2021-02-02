0827685f477523349db923c53c94697f278529fc
Red_StonixX <72993807+RedSotnixX@users.noreply.github.com>
2021-02-02 17:58:08 +0100

    update

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const config = require('./config.json');
 const bdd = require("./bdd.json");
 
 const roleClaim = require('./commands/role-claim')
-const poll = require('./commands/poll')
-const poll2 = require('./commands/poll2')
+//const poll = require('./commands/poll')
 const memberCount = require('./commands/member-count')
 
 const fs = require("fs");
@@ -25,8 +24,7 @@ bot.on("ready", async () => {
     ChannelReady.send('Je suis lancé');
 
     //memberCount(bot)
-    poll2()
-    poll(bot)
+    //poll(bot)
     roleClaim(bot)
 })
 
@@ -152,6 +150,26 @@ bot.on('message', message => {
   }
   }
 ) */
+
+//  Test aide //
+
+bot.on("message", async message => {
+  if(message.content === `${prefix}stest`) {
+    message.channel.send(new Discord.MessageEmbed()
+            .addField('Nom', message.guild.name, true)
+            .addField('Région', message.guild.region, true)
+            .addField('Membres', `${message.guild.memberCount} membres\n${message.guild.members.cache.filter(member => !member.user.bot).size} humains\n${message.guild.members.cache.filter(member => member.user.bot).size} bots`, true)
+            .addField('Salons', `${message.guild.channels.cache.size} salons\n${message.guild.channels.cache.filter(channel => channel.type === 'text').size} salons textuels\n${message.guild.channels.cache.filter(channel => channel.type === 'voice').size} salons vocaux\n${message.guild.channels.cache.filter(channel => channel.type === 'category').size} catégories`, true)
+            .addField('Emojis', `${message.guild.emojis.cache.size} emojis\n${message.guild.emojis.cache.filter(emoji => !emoji.animated).size} emojis statiques\n${message.guild.emojis.cache.filter(emoji => emoji.animated).size} emojis animés`, true)
+            .addField('Rôles', message.guild.roles.cache.size, true)
+            .addField('Propriétaire', message.guild.owner, true)
+            .addField('Date de création', moment(message.guild.createdAt).format('[Le] DD/MM/YYYY [à] HH:mm:ss'), true)
+            .addField('Nitro boost', `Tier : ${message.guild.premiumTier}\nNombre de boosts : ${message.guild.premiumSubscriptionCount}`, true)
+            .setFooter(`ID : ${message.guild.id}`)
+            .setThumbnail(message.guild.iconURL())
+            .setImage(message.guild.bannerURL()))
+  }
+});
 
 //  LIEN INVITATION //
 
@@ -413,5 +431,5 @@ function play(guild, song) {
   serverQueue.textChannel.send(`Start playing: **${song.title}**`);
 } */
 
-bot.login(process.env.BOT_TOKEN)
-//bot.login('TOKEN')
+//bot.login(process.env.BOT_TOKEN)
+bot.login('NzQzMTgyNTM2MTUwMDg5ODYw.XzQ8rA.QS-gw2jcZ2Xv0lmaARZrJj7ugV8')
