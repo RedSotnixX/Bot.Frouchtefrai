0da6c44bd2fa2a9863cfc96c8ff6f4295047e715
Red_StonixX <72993807+RedSotnixX@users.noreply.github.com>
2021-02-05 16:54:03 +0100

    Update

--- commands/member-count-pc.js
+++ commands/member-count-pc.js
@@ -1,14 +1,14 @@
 module.exports = (bot) => {
-    const channelId = '805168671781027872'
+    const channelId = '806258130974801952'
   
     const updateMembers = (guild) => {
       const channel = guild.channels.cache.get(channelId)
-      channel.setName(`Membres : ${guild.members.cache.filter(member => !member.user.bot).size}`);
+      channel.setName(`On est : ${guild.members.cache.filter(member => !member.user.bot).size} membres`);
     }
   
     bot.on('guildMemberAdd', (member) => updateMembers(member.guild))
     bot.on('guildMemberRemove', (member) => updateMembers(member.guild))
   
-    const guild = bot.guilds.cache.get('717351839963545639')
+    const guild = bot.guilds.cache.get('806209941660106762')
     updateMembers(guild)
 }
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bdd = require("./bdd.json");
 const roleClaim = require('./commands/role-claim')
 const poll = require('./commands/poll')
 const memberCount = require('./commands/member-count')
+const memberCountPc = require('./commands/member-count-pc')
 
 const fs = require("fs");
 const moment = require('moment');
@@ -23,7 +24,8 @@ bot.on("ready", async () => {
     var ChannelReady = bot.channels.cache.get('717351840550879275');
     ChannelReady.send('Je suis lancé');
 
-    //memberCount(bot)
+    memberCountPc(bot)
+    memberCount(bot)
     poll(bot)
     roleClaim(bot)
 })
@@ -95,341 +97,211 @@ bot.giveawaysManager = new GiveawaysManager(bot, {
 });
 
 
-//  COMMANDE DE STATS  //
+//  BIENVENUE 1-G1 Spé PC ///
+
+bot.on("guildMemberAdd", (member, guild) => {
+  if (guild.id !== "806209941660106762") return;
+  
+  var channel = bot.channels.cache.get('717351840550879275');
+  channel.send('Une personne a rejoin 1-G1 Spé PC');
+});
+
+
+
+///     COMMANDE MESSAGE      ///
 
 bot.on('message', message => {
-  if (message.content.startsWith(`${prefix}stats`)) {
-    let onlines = message.guild.members.cache.filter(member => !member.user.bot && member.presence.status !== "offline").size;
-    let totalmembers = message.guild.members.cache.filter(member => !member.user.bot).size;
-    let totalservers = bot.guilds.cache.size;
-    let totalbots = message.guild.members.cache.filter(member => member.user.bot).size;
-    /* let total_news = message.guild.roles.cache.get('ID_ROLE_DES_NOUVEAUX_MEMBRES').members.size; */
-  
-    const EmbedStats = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Code source')
-        .setURL('https://github.com/RedStonixX')
-        .setAuthor('Bot Frouchtefrai', 'https://cdn.manomano.com/images/sheets/legrand-prise-de-courant-avec-terre-neptune-blanc-P-521680-1684965_1.jpg', 'https://discord.js.org')
-        .setDescription('Voici les statistiques')
-        //.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-        .addFields({
-            name: 'Nombre de membres sur le serveur',
-            value: totalmembers,
-            inline: true
-        }, {
-            name: 'Nombres de bots sur le serveur : ',
-            value: totalbots,
-            inline: true
-        }, {
-            name: 'Membres connéctés : ',
-            value: onlines,
-            inline: true
-        }, {
-            name: 'Nombre de serveurs auquel le bot appartient : ',
-            value: totalservers,
-            inline: true
-        }, /* {
-            name: 'Nombre d\'arrivants : ',
-            value: total_news,
-            inline: true
-        },  */)
-        //.setImage('https://i.imgur.com/wSTFkRM.png')
-        .setTimestamp()
-        .setFooter('Red_StonixX', 'https://cdn.discordapp.com/avatars/491312416098091028/093d164ecfc547ab88f12d93e93ac48f.webp?size=1024');
-  
-    message.channel.send(EmbedStats);
-  }
-})
+
+
+// COMMANDE DE STATS  //
+
+
+if (message.content.startsWith(`${prefix}stats`)) {
+  let onlines = message.guild.members.cache.filter(member => !member.user.bot && member.presence.status !== "offline").size;
+  let totalmembers = message.guild.members.cache.filter(member => !member.user.bot).size;
+  let totalservers = bot.guilds.cache.size;
+  let totalbots = message.guild.members.cache.filter(member => member.user.bot).size;
+  /* let total_news = message.guild.roles.cache.get('ID_ROLE_DES_NOUVEAUX_MEMBRES').members.size; */
+
+  const EmbedStats = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Code source')
+      .setURL('https://github.com/RedStonixX')
+      .setAuthor('Bot Frouchtefrai', 'https://cdn.manomano.com/images/sheets/legrand-prise-de-courant-avec-terre-neptune-blanc-P-521680-1684965_1.jpg', 'https://discord.js.org')
+      .setDescription('Voici les statistiques')
+      //.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+      .addFields({
+          name: 'Nombre de membres sur le serveur',
+          value: totalmembers,
+          inline: true
+      }, {
+          name: 'Nombres de bots sur le serveur : ',
+          value: totalbots,
+          inline: true
+      }, {
+          name: 'Membres connéctés : ',
+          value: onlines,
+          inline: true
+      }, {
+          name: 'Nombre de serveurs auquel le bot appartient : ',
+          value: totalservers,
+          inline: true
+      }, /* {
+          name: 'Nombre d\'arrivants : ',
+          value: total_news,
+          inline: true
+      },  */)
+      //.setImage('https://i.imgur.com/wSTFkRM.png')
+      .setTimestamp()
+      .setFooter('Red_StonixX', 'https://cdn.discordapp.com/avatars/491312416098091028/093d164ecfc547ab88f12d93e93ac48f.webp?size=1024');
+
+  message.channel.send(EmbedStats);
+}
+
 
 //  ELIRION ADD MODO  //
 
-/* bot.on('message', message => {
-  if(message.content === `${prefix}modo` && message.author.id === '491312416098091028'){
-    member = message.guild.members.cache.get(String(message.author.id))
-    member.roles.add("536246625631338496");
-  }
-  }
-) */
 
-//  Test aide //
+if(message.content === `${prefix}modo` && message.author.id === '491312416098091028'){
+  member = message.guild.members.cache.get(String(message.author.id))
+  member.roles.add("536246625631338496");
+}
 
-/* bot.on("message", async message => {
-  if(message.content === `${prefix}stest`) {
-    message.channel.send(new Discord.MessageEmbed()
-            .addField('Nom', message.guild.name, true)
-            .addField('Région', message.guild.region, true)
-            .addField('Membres', `${message.guild.memberCount} membres\n${message.guild.members.cache.filter(member => !member.user.bot).size} humains\n${message.guild.members.cache.filter(member => member.user.bot).size} bots`, true)
-            .addField('Salons', `${message.guild.channels.cache.size} salons\n${message.guild.channels.cache.filter(channel => channel.type === 'text').size} salons textuels\n${message.guild.channels.cache.filter(channel => channel.type === 'voice').size} salons vocaux\n${message.guild.channels.cache.filter(channel => channel.type === 'category').size} catégories`, true)
-            .addField('Emojis', `${message.guild.emojis.cache.size} emojis\n${message.guild.emojis.cache.filter(emoji => !emoji.animated).size} emojis statiques\n${message.guild.emojis.cache.filter(emoji => emoji.animated).size} emojis animés`, true)
-            .addField('Rôles', message.guild.roles.cache.size, true)
-            .addField('Propriétaire', message.guild.owner, true)
-            .addField('Date de création', moment(message.guild.createdAt).format('[Le] DD/MM/YYYY [à] HH:mm:ss'), true)
-            .addField('Nitro boost', `Tier : ${message.guild.premiumTier}\nNombre de boosts : ${message.guild.premiumSubscriptionCount}`, true)
-            .setFooter(`ID : ${message.guild.id}`)
-            .setThumbnail(message.guild.iconURL())
-            .setImage(message.guild.bannerURL()))
-  }
-}); */
 
 //  LIEN INVITATION //
 
-bot.on('message', message => {
-  if (message.content === `${prefix}invite`) {
-    message.channel.send('https://discord.com/api/oauth2/authorize?client_id=743182536150089860&permissions=8&scope=bot')
-  }
-})
+
+if (message.content === `${prefix}invite`) {
+  message.channel.send('https://discord.com/api/oauth2/authorize?client_id=743182536150089860&permissions=8&scope=bot')
+}
+
 
 //  SKYPE PHYSIQUE  //
 
-bot.on('message', message => {
-  if(message.content === `${prefix}physique` && message.author.id === '695314219016519701'){
-    message.channel.send('https://join.skype.com/pIwnF5fYtYHP')}
+
+if(message.content === `${prefix}physique` && message.author.id === '695314219016519701'){
+  message.channel.send('https://join.skype.com/pIwnF5fYtYHP')
+}
+
+
+//  MESSAGE SIMPLE  //
+
+
+if (message.content === `${prefix}1`) {
+  message.channel.send('et 2 et 3')
+}
+
+
+//  Réponse à @Frouchtefrai //
+
+
+if (message.mentions.users.first() == bot.user.id) {
+  message.reply('il y a un problème ?')
+}
+
+
+//  MESSAGE SIMPLE  //
+
+
+if(message.content === `${prefix}2` && message.author.id === '491312416098091028'){
+  message.channel.send('YEET')
+}
+
+
+//  INFO SERV SIMPLE  //
+
+
+if (message.content === `${prefix}serveur`) {
+  message.channel.send(`Nom du serveur: ${message.guild.name}\nNombre total de membres: ${message.guild.memberCount}`);
+}
+
+
+//  COMMENCE AVEC LE PREFIX //
+
+
+if (message.content.startsWith(`${prefix}pingpong`)) {
+  message.channel.send('je suis là :D')
+}
+
+
+//  INFO UTILISATEUR  //
+
+
+if (message.content.startsWith(`${prefix}info`)) {
+  if(message.mentions.users.first()) {
+      user = message.mentions.users.first();
+ } else{
+      user = message.author;
   }
-)
-
-//  MESSAGE DE REPONSE  //
-
-/* bot.on('message', message => {
-    if (message.content === `${prefix}1`) {
-      message.channel.send('et 2 et 3')
-    }
-  }) */
-
-bot.on('message', message => {
-    if (message.mentions.users.first() == bot.user.id) {
-      message.reply('il y a un problème ?')
-    }
-  })
-
-/* bot.on('message', message => {
-    if(message.content === `${prefix}2` && message.author.id === '491312416098091028'){
-      message.channel.send('YEET')}
-    }
-  ) */
-
-/* bot.on('message', message => {
-    if (message.content === `${prefix}moi`) {
-      message.channel.send(`Nom d'utilisateur: ${message.author.username}\nTon ID: ${message.author.id}`);
-    }
-  }) */
-
-bot.on('message', message => {
-    if (message.content === `${prefix}serveur`) {
-      message.channel.send(`Nom du serveur: ${message.guild.name}\nNombre total de membres: ${message.guild.memberCount}`);
-    }
-  })
-
-// COMMENCE AVEC LE PREFIX  //
-
-// bot.on('message', message => {
-//     if (message.content.startsWith(`${prefix}ping`)) {
-//       message.channel.send('je suis là :D')
-//     }
-//   })
-
-//  INFO  //
-
-bot.on('message', message => {
-  if (message.content.startsWith(`${prefix}info`)) {
-    if(message.mentions.users.first()) {
-        user = message.mentions.users.first();
-   } else{
-        user = message.author;
-    }
-    const member = message.guild.member(user);
-    console.log(user);
-    const embed = new Discord.MessageEmbed() 
-    .setColor('#ff5555')
-    .setThumbnail(user.avatarURL)
-    .setTitle(`Information sur ${user.username}#${user.discriminator} :`)
-    .addField('ID du compte:', `${user.id}`, true)
-    .addField('Pseudo sur le serveur :', `${member.nickname ? member.nickname : 'Aucun'}`, true)
-    .addField('A crée son compte le :', `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-    .addField('A rejoint le serveur le :', `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-    .addField('Status:', `${user.presence.status}`, true)
-    .addField('Joue a :', `${user.presence.activities[0] ? user.presence.activities[0].name : 'Rien'}`, true)
-    .addField('Roles :', member.roles.cache.map(roles => `${roles.name}`).join(', '), true)
-    .addField(`En réponse a :`,`${message.author.username}#${message.author.discriminator}`)
+  const member = message.guild.member(user);
+  console.log(user);
+  const embed = new Discord.MessageEmbed() 
+  .setColor('#ff5555')
+  .setThumbnail(user.avatarURL)
+  .setTitle(`Information sur ${user.username}#${user.discriminator} :`)
+  .addField('ID du compte:', `${user.id}`, true)
+  .addField('Pseudo sur le serveur :', `${member.nickname ? member.nickname : 'Aucun'}`, true)
+  .addField('A crée son compte le :', `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+  .addField('A rejoint le serveur le :', `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+  .addField('Status:', `${user.presence.status}`, true)
+  .addField('Joue a :', `${user.presence.activities[0] ? user.presence.activities[0].name : 'Rien'}`, true)
+  .addField('Roles :', member.roles.cache.map(roles => `${roles.name}`).join(', '), true)
+  .addField(`En réponse a :`,`${message.author.username}#${message.author.discriminator}`)
 message.channel.send(embed).then(message => message.delete({ timeout: 30000 }));
 }
-})
+
 
 //  MESSAGE DE BIENVENUE  //
 
-/* bot.on("guildMemberAdd", member => {
-  if (bdd["message-bienvenue"]) {
-      bot.channels.cache.get('ID_CHANNEL_DE_BIENVENUE').send(bdd["message-bienvenue"] + `${member.user.username}`);
-  } else {
-      bot.channels.cache.get('ID_CHANNEL_DE_BIENVENUE').send("Bienvenue sur le serveur");
-  }
-  member.roles.add('ID_ROLE_DE_BIENVENUE');
 
-}) */
+/* if (bdd["message-bienvenue"]) {
+  bot.channels.cache.get('ID_CHANNEL_DE_BIENVENUE').send(bdd["message-bienvenue"] + `${member.user.username}`);
+} else {
+  bot.channels.cache.get('ID_CHANNEL_DE_BIENVENUE').send("Bienvenue sur le serveur");
+}
+member.roles.add('ID_ROLE_DE_BIENVENUE'); */
 
-/* bot.on('message', message => {
-  if(message.content.startsWith(`f/b`)){
-    message.delete()
-    if(message.member.hasPermission('MANAGE_MESSAGES')){
-      if(message.content.length > 5){
-        message_bienvenue = message.content.slice(4)
-        bdd["message-bienvenue"] = message_bienvenue
-        Savebdd()
-      }
+
+/* if(message.content.startsWith(`f/b`)){
+  message.delete()
+  if(message.member.hasPermission('MANAGE_MESSAGES')){
+    if(message.content.length > 5){
+      message_bienvenue = message.content.slice(4)
+      bdd["message-bienvenue"] = message_bienvenue
+      Savebdd()
     }
   }
-}) */
-
-//test
-
-bot.on('message', message => {
-  if(message.content === `${prefix}test` && message.author.id === '491312416098091028'){
-    message.reply(bdd["message-bienvenue"])}
-  }
-)
-
-//  PING   //
-
-bot.on('message', message => {
-  if(message.content === `${prefix}ping`) {
-  
-    // It sends the user "Pinging"
-          message.channel.send("Pinging...").then(m =>{
-            // The math thingy to calculate the user's ping
-              var ping = m.createdTimestamp - message.createdTimestamp;
-  
-            // Basic embed
-              var embed = new Discord.MessageEmbed()
-              .setAuthor(`Your ping is ${ping} ms`)
-              .setColor("Your Color")
-              
-              // Then It Edits the message with the ping variable embed that you created
-              m.edit(embed)
-          });
-      }
-})
-
-//  MUSIQUE MARCHE PAS SUR HEROKU //
-
-/* const ytdl = require("ytdl-core");
-
-bot.on('message', message => {
-  const serverQueue = queue.get(message.guild.id);
-  if (message.content.startsWith(`${prefix}play`)) {
-      execute(message, serverQueue, args);
-      return;
-  } 
-  if (message.content.startsWith(`${prefix}skip`)) {
-      skip(message, serverQueue, args);
-      return;
-  } 
-  if (message.content.startsWith(`${prefix}stop`)) {
-      stop(message, serverQueue, args);
-      return;
-  } 
-  if (message.content.startsWith(`${prefix}pause`)) {
-      pause(message, serverQueue, args);
-      return;
-  } 
-})
-
-async function execute(message, serverQueue, args) {
-  const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel)
-      return message.channel.send(
-          "You need to be in a voice channel to play music!"
-      );
-  const permissions = voiceChannel.permissionsFor(message.client.user);
-  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-      return message.channel.send(
-          "I need the permissions to join and speak in your voice channel!"
-      );
-  }
-
-  const songInfo = await ytdl.getInfo(args[1]);
-  const song = {
-      title: songInfo.title,
-      url: songInfo.video_url
-  };
-
-  if (!serverQueue) {
-      const queueContruct = {
-          textChannel: message.channel,
-          voiceChannel: voiceChannel,
-          connection: null,
-          songs: [],
-          volume: 5,
-          playing: true
-      };
-
-      queue.set(message.guild.id, queueContruct);
-
-      queueContruct.songs.push(song);
-
-      try {
-          var connection = await voiceChannel.join();
-          queueContruct.connection = connection;
-          play(message.guild, queueContruct.songs[0]);
-      } catch (err) {
-          console.log(err);
-          queue.delete(message.guild.id);
-          return message.channel.send(err);
-      }
-  } else {
-      serverQueue.songs.push(song);
-      return message.channel.send(`${song.title} has been added to the queue!`);
-  }
-}
-
-function skip(message, serverQueue, args) {
-  if (!message.member.voice.channel)
-      return message.channel.send(
-          "You have to be in a voice channel to stop the music!"
-      );
-  if (!serverQueue)
-      return message.channel.send("There is no song that I could skip!");
-  serverQueue.connection.dispatcher.end();
-}
-
-function stop(message, serverQueue, args) {
-  if (!message.member.voice.channel)
-      return message.channel.send(
-          "You have to be in a voice channel to stop the music!"
-      );
-  serverQueue.songs = [];
-  serverQueue.connection.dispatcher.end();
-}
-
-function pause(message, serverQueue, args) {
-  if (!message.member.voice.channel)
-      return message.channel.send(
-          "You have to be in a voice channel to stop the music!"
-      );
-  if (serverQueue.connection.dispatcher.paused) {
-      serverQueue.connection.dispatcher.resume();
-  } else {
-      serverQueue.songs = [];
-      serverQueue.connection.dispatcher.pause();
-  }
-}
-
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id);
-  if (!song) {
-      serverQueue.voiceChannel.leave();
-      queue.delete(guild.id);
-      return;
-  }
-
-  const dispatcher = serverQueue.connection
-      .play(ytdl(song.url))
-      .on("finish", () => {
-          serverQueue.songs.shift();
-          play(guild, serverQueue.songs[0]);
-      })
-      .on("error", error => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
 } */
 
-bot.login(process.env.BOT_TOKEN)
-//bot.login('TOKEN')
+
+/* if(message.content === `${prefix}test` && message.author.id === '491312416098091028'){
+  message.reply(bdd["message-bienvenue"]
+  )
+} */
+
+
+//  PING  //
+
+
+if(message.content === `${prefix}ping`) {
+  
+  // It sends the user "Pinging"
+        message.channel.send("Pinging...").then(m =>{
+          // The math thingy to calculate the user's ping
+            var ping = m.createdTimestamp - message.createdTimestamp;
+
+          // Basic embed
+            var embed = new Discord.MessageEmbed()
+            .setAuthor(`Your ping is ${ping} ms`)
+            .setColor("Your Color")
+            
+            // Then It Edits the message with the ping variable embed that you created
+            m.edit(embed)
+        });
+    }
+
+
+})
+
+//bot.login(process.env.BOT_TOKEN)
+bot.login('NzQzMTgyNTM2MTUwMDg5ODYw.XzQ8rA.bmW3IG4JvRFw7oHxIGWqtLeXie8')
